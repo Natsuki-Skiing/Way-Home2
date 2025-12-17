c7af4edafb6ef893fa2f73476a366257d82fae2f
Bill-Clinton <willjames1109@outlook.com>
2025-12-17 22:06:11 +0000

    Made Life leach enchantment effect

--- src/main/java/items/EnchantmentEffects/LifeLeachEffect.java
+++ src/main/java/items/EnchantmentEffects/LifeLeachEffect.java
@@ -4,8 +4,8 @@ import items.EnchantmentEffect;
 public class LifeLeachEffect implements EnchantmentEffect{
     @Override 
     public String apply(creatures.Entity attacker, creatures.Entity target, double magnitude) {
-        double damageDealt = target.takeDamage(magnitude);
-        attacker.heal(damageDealt);
+        double damageDealt = target.subHp(magnitude);
+        attacker.addHp(damageDealt);
         return String.format("%s leaches %.2f health from %s.", attacker.getName(), damageDealt, target.getName());
     }
 }  
