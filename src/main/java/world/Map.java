57b4449006ce563943705d33c5e67f9700fc5fda
Bill-Clinton <willjames1109@outlook.com>
2026-03-28 14:31:37 +0000

    Added a noise function to map gen region picker to make it better

--- src/main/java/world/Map.java
+++ src/main/java/world/Map.java
@@ -1,11 +1,18 @@
 package world;
 import java.util.HashMap;
 import interfaces.GameTile;
+import enums.worldRegionEnum;
 public class Map {
     private HashMap<String,GameTile> mapTiles;
-    
+
+
+    public Map(int width, int height,worldRegionEnum region){
+
+        
+
+    }
     private String mapKey(int x , int y){
-        return((Integer.toString(x)+Integer.toString(y)));
+        return((Integer.toString(x)+","+Integer.toString(y)));
     }
     public GameTile getMapTile(int x, int y){
         return(this.mapTiles.get(mapKey(x, y)));
--- src/main/java/world/World.java
+++ src/main/java/world/World.java
@@ -1,38 +1,55 @@
 package world;
 import java.util.HashMap;
 import enums.worldRegionEnum;
+import util.FastNoiseLite;
+import util.FastNoiseLite.NoiseType;
 public class World {
     //I need to load already made maps here
     private HashMap<String,Map> mapsHash;
     private int mapWidth;
     private int mapHeight;
-
+    private FastNoiseLite noiseMaker;
 
     public World(int mapWidth, int mapHeight,String existingMapsJsonPath){
         //TODO load edisting maps 
 
         this.mapWidth = mapWidth;
         this.mapHeight = mapHeight;
+
+        // Noise maker for the map type generation
+        this.noiseMaker = new FastNoiseLite();
+        this.noiseMaker.SetSeed(1337);
+        this.noiseMaker.SetNoiseType(NoiseType.OpenSimplex2);
+
     }
 
 
-    
+    private String getMapKey(int x, int y){
+        return((Integer.toString(x)+","+Integer.toString(y)));
+    }
 
     private void genMap(int x,int y){
-        worldRegionEnum regionEnum = worldRegionEnum.NORMAL;
-        if(y > 10){
+        float frequency = 0.05f;
+
+        float noiseValue = this.noiseMaker.GetNoise(x*frequency, y*frequency);
+
+        worldRegionEnum regionEnum;
+        if (noiseValue < -0.4f) {
             regionEnum = worldRegionEnum.SNOW;
-        }else if(y <-10){
-            regionEnum = worldRegionEnum.DESERT;
-        }else if(x < -10 && y <10 && y >-10 ){
-            regionEnum = worldRegionEnum.BOG;
-        }else if(x > 10  && y <10 && y >-10){
+        } else if (noiseValue < -0.1f) {
             regionEnum = worldRegionEnum.MOUNTAINS;
+        } else if (noiseValue < 0.2f) {
+            regionEnum = worldRegionEnum.NORMAL;
+        } else if (noiseValue < 0.5f) {
+            regionEnum = worldRegionEnum.BOG;
+        } else {
+            regionEnum = worldRegionEnum.DESERT;
         }
 
-        String mapKey = Integer.toString(x) + Integer.toString(y);
+        String mapKey = getMapKey(x, y);
 
-        Map newMap = Map(this.mapWidth,this.mapHeight,regionEnum);
+        //Actually making the map
+        Map newMap = new Map(this.mapWidth,this.mapHeight,regionEnum);
 
         //Might do more stuff here 
 
@@ -41,7 +58,7 @@ public class World {
     
     }
     public Map getMap(int worldX, int worldY){
-        String mapKey = Integer.toString(worldX) + Integer.toString(worldY);
+        String mapKey = getMapKey(worldX, worldY);
 
         if(!this.mapsHash.containsKey(mapKey)){
             //generate a map
