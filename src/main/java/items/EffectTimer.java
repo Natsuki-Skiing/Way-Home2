18b0d0c681df45fa3ac8dfbb44cd7386a1c854aa
William <will@unknown.home>
2026-04-25 17:27:16 +0100

    Making class to track effects lifetime

---
+++ src/main/java/items/EffectTimer.java
@@ -0,0 +1,37 @@
+package items;
+import enums.effectCounterType;
+public class EffectTimer {
+    private int effectCounter;
+    private effectCounterType type;
+
+    public int getCounter(){
+        return(this.effectCounter);
+    }
+
+    public int decrementCounter(){
+        if(this.effectCounter > 0){
+            this.effectCounter --;
+        }
+
+        return(this.effectCounter);
+    }
+
+    public int decrementCounter(effectCounterType type){
+        if(this.type == type){
+            return(decrementCounter());
+        }
+        return(-1);
+    }
+
+    public effectCounterType getType(){
+        return(this.type);
+    }
+
+    public boolean active(){
+        if(this.effectCounter >0){
+            return(true);
+        }
+
+        return(false);
+    }
+}
